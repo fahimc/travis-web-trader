fe790e1e42e5581d966f9277b3d6dc08534417a6
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2017-05-18 21:57:27 +0100

    improved bull prediction

--- dist/js/bullishPrediction.js
+++ dist/js/bullishPrediction.js
@@ -1,18 +1,19 @@
 const BullishPrediction = {
     predict(ticks) {
         if ( Main.isProposal || Main.pauseTrading) return;
+        let lastTick = ticks[ticks.length-4];
         let previousTick = ticks[ticks.length-2];
         let currentTick = ticks[ticks.length-1];
         let highest = currentTick;
         let lowest = currentTick;
         let found= false;
-        if (previousTick < currentTick) {
+        if (lastTick < previousTick && previousTick < currentTick) {
             proposal = 'CALL';
             predictionType = 'BULL_UP';
             found = true;
             highest = currentTick;
             lowest = previousTick;
-        } else if (previousTick > currentTick) {
+        } else if (lastTick > previousTick && previousTick > currentTick) {
             proposal = 'PUT';
             predictionType = 'BULL_DOWN';
             found = true;
@@ -25,7 +26,7 @@ const BullishPrediction = {
                 predictionType: predictionType,
                 type: proposal
             };
-            ChartComponent.updatePredictionChart([previousTick,currentTick], lowest, highest);
+            ChartComponent.updatePredictionChart(ticks.slice(ticks.length-4,ticks.length), lowest, highest);
             Main.setPrediction(proposal, predictionType);
         }
 
