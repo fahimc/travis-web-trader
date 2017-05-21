dc15f37e5b14bf2a68a4193ef2a0e657196bc340
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2017-05-21 23:34:30 +0100

    updated models

--- dist/js/model/asset/RDBULL.js
+++ dist/js/model/asset/RDBULL.js
@@ -1,6 +1,6 @@
 var RDBULLModel = {
-  chartBarrier:1,
-  priceChangeBarrier:1,
+  chartBarrier:0.2,
+  priceChangeBarrier:0.5,
   payout:{
     CALL:0.901,
     PUT:0.984
--- dist/js/model/asset/R_10.js
+++ dist/js/model/asset/R_10.js
@@ -1,7 +1,7 @@
 var R_10Model = {
-  chartBarrier:0.5,
+  chartBarrier:0.1,
   volatilityBarrier:10,
-  priceChangeBarrier:0.5,
+  priceChangeBarrier:0.3,
   payout:{
     CALL:0.94,
     PUT:0.94
